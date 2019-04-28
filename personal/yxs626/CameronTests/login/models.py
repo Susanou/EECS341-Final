6ce2fc57f1104af04c3466a7893676053b9242e7
GenTsuki <gentsk77@gmail.com>
2019-04-28 00:43:56 -0400

    model class MemberLevel migrated

--- personal/yxs626/CameronTests/login/models.py
+++ personal/yxs626/CameronTests/login/models.py
@@ -23,6 +23,30 @@ class Staff(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
     name = models.CharField(max_length = 100)
 
+class MemberLevel(models.Model):
+    name = (
+        ('B', 'Bronze'),
+        ('S', 'Silver'),
+        ('G', 'Gold'),
+        ('P', 'Platinum'),
+        ('D', 'Diamond'), 
+    )
+    level_status = models.CharField(max_length=1, choices=name, default='B', primary_key=True)
+    price = models.IntegerField()
+
+class Class(models.Model):
+    name = models.CharField(max_length = 50)
+    location = models.CharField(max_length=50)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
+    classes = models.ForeignKey(Class, on_delete = models.CASCADE)
+    level = models.ForeignKey(MemberLevel, default='B', on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
 
 class Membership(models.Model):
     '''
@@ -31,19 +55,4 @@ class Membership(models.Model):
     funds = models.IntegerField()
     length = models.IntegerField()
     #length = models.DateField()
-
-
-class Class(models.Model):
-    name = models.CharField(max_length = 50)
-    location = models.CharField(max_length=50)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
-
-
-class Member(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
-    classes = models.ForeignKey(Class, on_delete = models.CASCADE)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
 
