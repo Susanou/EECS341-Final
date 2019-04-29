87d50ac7f8d4f6b484742eb0e448a9a6894dec05
GenTsuki <gentsk77@gmail.com>
2019-04-28 20:39:39 -0400

    levels added to database, model membership deleted, attributes merged into member

--- personal/yxs626/CameronTests/login/models.py
+++ personal/yxs626/CameronTests/login/models.py
@@ -32,7 +32,7 @@ class MemberLevel(models.Model):
         ('D', 'Diamond'), 
     )
     level_status = models.CharField(max_length=1, choices=name, default='B', primary_key=True)
-    price = models.IntegerField()
+    price = models.FloatField()
 
 class Class(models.Model):
     name = models.CharField(max_length = 50)
@@ -56,14 +56,11 @@ class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
     classes = models.ManyToManyField(Class)
     level = models.ForeignKey(MemberLevel, default='B', on_delete = models.CASCADE)
+    funds = models.FloatField(default=0)
+    #changed length to expired date, default to a month from now 
+    expired_date = models.DateField(default=date(datetime.now().year, 
+                                    datetime.now().month, datetime.now().day) + timedelta(days=30))
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
-
-class Membership(models.Model):
-    funds = models.IntegerField()
-    #changed length to expired date, default to a month from now 
-    expired_date = models.DateField(default=date(datetime.now().year, 
-                                    datetime.now().month, datetime.now().day) + timedelta(days=30))
-
--- personal/yxs626/CameronTests/profile/admin.py
+++ personal/yxs626/CameronTests/profile/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from login.models import Member, Staff, Class
+from login.models import Member, Staff, Class, MemberLevel, User
 
 admin.site.register(Member)
 admin.site.register(Staff)
 admin.site.register(Class)
+admin.site.register(User)
+admin.site.register(MemberLevel)
