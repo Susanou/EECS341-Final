577d4414773e43db9c2d0a7c487452c102d0602e
GenTsuki <gentsk77@gmail.com>
2019-04-29 02:06:34 -0400

    base html changed, basic info of profile.html displayed

--- personal/yxs626/CameronTests/profile/urls.py
+++ personal/yxs626/CameronTests/profile/urls.py
@@ -7,5 +7,7 @@ app_name = 'profile'
 urlpatterns = [
     # "/polls" is the prefix
     path('', views.index, name='index'),
-
+    path('<int:class_id>/class/', views.classInfo, name='class'),
+    path('classlist', views.classlist, name='classlist'),
+    path('staff', views.staff, name='staff'),
 ]
--- personal/yxs626/CameronTests/profile/views.py
+++ personal/yxs626/CameronTests/profile/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 
-from login.models import Member
+from login.models import Member, Class, Staff, MemberLevel
 
 # Create your views here.
 
@@ -11,11 +11,41 @@ from login.models import Member
 def index(request):
     try:
         # need to adjust given input
-        member = Member.objects.get(name='Melody')
+        mname = 'GenTsuki'
+        #using raw sql
+        member = Member.objects.raw('SELECT * FROM login_member m WHERE m.name = %s', [mname])[0]
+        classes = Class.objects.raw('SELECT c.id, c.name, c.location, c.staff_id FROM login_class c, login_member m WHERE m.name = %s AND m.classes_id = c.id', [mname])
+        level = MemberLevel.objects.raw('SELECT l.level_status, l.price FROM login_memberlevel l, login_member m WHERE m.name = %s AND m.level_id = l.level_status', [mname])[0]
     except Member.DoesNotExist:
         raise Http404("no such member")
     print(member)
     context = {
-        'member' : member, 
+        'member' : member,
+        'classes': classes,
+        'level': level,
     }
     return render(request, 'profile/index.html', context)
+
+def classInfo(request, class_id):
+    class_object = get_object_or_404(Class, pk=class_id)
+    return render(request,'profile/class.html',{'class':class_object})
+
+def classlist(request):
+    classlist = Class.objects.all()
+    context = {
+        'classlist': classlist,
+    }
+    return render (request, 'profile/classlist.html',context)
+
+def staff(request):
+    try:
+        # need to adjust given input
+        staff = Staff.objects.get(name='Melody')
+        classes = Class.objects.filter(staff=staff)
+    except Class.DoesNotExist:
+        raise Http404("no such member")
+    context = {
+        'staff' : staff,
+        'classes': classes,
+    }
+    return render(request, 'profile/staff.html', context)
