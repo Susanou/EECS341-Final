3a71e06ba0830e403a0765ea3976ef6678bc8398
Fukurou <caos2804@gmail.com>
2019-05-04 18:05:26 -0400

    UrlFix, login only work for superusers

--- FitHub/login/models.py
+++ FitHub/login/models.py
@@ -71,7 +71,7 @@ class Member(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, null=True) # validators should be a list
 
     def __str__(self):
         return self.name
